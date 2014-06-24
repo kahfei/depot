f0110c4c6714b7c3f5abc1b1479461ba043aa607
kahfei <bak.kahfei@gmail.com>
2014-06-25 02:10:41 +0800

    completing cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -57,9 +57,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1.json
   def destroy
     @cart.destroy
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to store_url, notice: 'You cart is currently empty' }
+      format.json { head :ok }
     end
   end
 
